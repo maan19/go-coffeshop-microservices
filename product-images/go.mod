c6334edc2dd0f0dcaa738a24d2fd5d5b839f8246
maan jagdeep <maanj872@gmail.com>
2023-01-24 16:22:30 -0800

    added file-save-handler

--- product-images/go.mod
+++ product-images/go.mod
@@ -3,12 +3,18 @@ module github.com/maan19/go-coffeshop-microservices/product-images
 go 1.19
 
 require (
+	github.com/gorilla/mux v1.8.0
+	github.com/hashicorp/go-hclog v1.4.0
 	github.com/stretchr/testify v1.8.1
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2
 )
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- product-images/handlers/files.go
+++ product-images/handlers/files.go
@@ -1,1 +1,51 @@
 package handlers
+
+import (
+	"net/http"
+	"path/filepath"
+
+	"github.com/gorilla/mux"
+	"github.com/hashicorp/go-hclog"
+	"github.com/maan19/go-coffeshop-microservices/product-images/files"
+)
+
+// Files is a handler for reading and writing files
+type Files struct {
+	log   hclog.Logger
+	store files.Storage
+}
+
+// NewFiles creates a new Files handler
+func NewFiles(l hclog.Logger, s files.Storage) *Files {
+	return &Files{
+		log:   l,
+		store: s,
+	}
+}
+
+// ServeHTTP implements the http.Handler interface
+func (f *Files) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fn := vars["filename"]
+
+	f.log.Info("Handle POST", "path", r.URL.Path, "id", id, "filename", fn)
+
+	//No need to check id and filename parameters for validation as it
+	//is already done by mux router
+
+	f.saveFile(id, fn, rw, r)
+
+}
+
+func (f *Files) saveFile(id string, filename string, rw http.ResponseWriter, r *http.Request) {
+	f.log.Info("Save file", "id", id, "filename", filename)
+
+	fp := filepath.Join(id, filename)
+	err := f.store.Save(fp, r.Body)
+	if err != nil {
+		f.log.Error("Failed to save file", "id", id, "filename", filename, "error", err)
+		http.Error(rw, "Unable to save file", http.StatusInternalServerError)
+		return
+	}
+}
